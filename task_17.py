24130186f3393521b240d13a277dac4530f68692
Vital Kudzelka <vital.kudzelka@gmail.com>
2020-05-06 20:09:03 +0300

    Add is_palindrome() task

---
+++ task_17.py
@@ -0,0 +1,13 @@
+"""
+Написать функцию которая примимает строку и проверяет является ли данная
+строка полиндромом, т.е. читается одинаково как слева направо, так и справа
+налево
+
+>>> is_palindrome("hello") == False
+>>> is_palindrome("AbbA") == True
+>>> is_palindrome("Never odd or even") == True
+"""
+
+
+def is_palindrome(input: str) -> bool:
+    ...
